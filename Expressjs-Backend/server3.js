83129def90d1d3464b366f8ea6815645d4defc00
Tanisha Gupta <tanishag1906@gmail.com>
2026-02-12 19:46:01 +0530

    Classwork 12-02-2026

--- Expressjs-Backend/server3.js
+++ Expressjs-Backend/server3.js
@@ -1,22 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const PORT = 5000;
+const PORT =4000;
 
-//to serve static files (CSS, JS, images)
-app.use(express.static('public'));
-app.use(express.json())
-app.use(express.urlencoded({ extended: true })); //to parse urlencoded data from forms
+app.set("view engine","ejs")
+//ejs - server site rendering
 
+app.use(express.urlencoded({extended: true}));
+const students=[
+    { id: 1, name: "rahul", branch: "CSE" },
+  { id: 2, name: "rishu", branch: "ECE" },
+  { id: 3, name: "ram", branch: "IT" },
+];
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/form.html');
-});
+app.get("/",(req,res)=>{
+    res.render("form",{allStudents: students});
 
-app.post('/students/register', (req, res) => {
-    console.log('Form data received:', req.body);
-    res.send('Form submitted successfully!');
-});
+  
+});                  
+
+app.post("/students/register",(req,res)=>{
+    console.log("form/data",req.body)
+    res.send("register")
+    
+})
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
